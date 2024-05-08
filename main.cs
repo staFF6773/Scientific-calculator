e11f18dee7493b496f9cc72ace306cfe36f59aa4
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-05-08 17:25:47 -0600

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,182 +1,210 @@
 using System;
 
+
 class Program
 {
-    static void Main()
-    {
-        while (true)
-        {
-            Console.Clear();
-            Console.WriteLine("Calculadora Científica Avanzada");
-            Console.WriteLine("1. Suma");
-            Console.WriteLine("2. Resta");
-            Console.WriteLine("3. Multiplicación");
-            Console.WriteLine("4. División");
-            Console.WriteLine("5. Seno");
-            Console.WriteLine("6. Coseno");
-            Console.WriteLine("7. Tangente");
-            Console.WriteLine("8. Arcoseno");
-            Console.WriteLine("9. Arcocoseno");
-            Console.WriteLine("10. Arcotangente");
-            Console.WriteLine("11. Exponencial");
-            Console.WriteLine("12. Logaritmo");
-            Console.WriteLine("13. Raíz cuadrada");
-            Console.WriteLine("14. Salir");
+  static void Main ()
+  {
+	while (true)
+	  {
+		Console.Clear ();
+		Console.WriteLine ("Calculadora CientC-fica Avanzada");
+		Console.WriteLine ("1. Suma");
+		Console.WriteLine ("2. Resta");
+		Console.WriteLine ("3. MultiplicaciC3n");
+		Console.WriteLine ("4. DivisiC3n");
+		Console.WriteLine ("5. Seno");
+		Console.WriteLine ("6. Coseno");
+		Console.WriteLine ("7. Tangente");
+		Console.WriteLine ("8. Arcoseno");
+		Console.WriteLine ("9. Arcocoseno");
+		Console.WriteLine ("10. Arcotangente");
+		Console.WriteLine ("11. Exponencial");
+		Console.WriteLine ("12. Logaritmo");
+		Console.WriteLine ("13. RaC-z cuadrada");
+		Console.WriteLine ("14. Salir");
 
-            Console.Write("Seleccione una opción: ");
-            string opcion = Console.ReadLine();
+		Console.Write ("Seleccione una opciC3n: ");
+		string opcion = Console.ReadLine ();
 
-            if (opcion == "14")
-                break;
+		if (opcion == "14")
+		  break;
 
-            double resultado = 0;
+		double resultado = 0;
 
-            switch (opcion)
-            {
-                case "1":
-                    resultado = OperacionBinaria(Suma);
-                    break;
-                case "2":
-                    resultado = OperacionBinaria(Resta);
-                    break;
-                case "3":
-                    resultado = OperacionBinaria(Multiplicacion);
-                    break;
-                case "4":
-                    resultado = OperacionBinaria(Division);
-                    break;
-                case "5":
-                    resultado = OperacionUnaria(Seno);
-                    break;
-                case "6":
-                    resultado = OperacionUnaria(Coseno);
-                    break;
-                case "7":
-                    resultado = OperacionUnaria(Tangente);
-                    break;
-                case "8":
-                    resultado = OperacionUnaria(Arcoseno);
-                    break;
-                case "9":
-                    resultado = OperacionUnaria(Arcocoseno);
-                    break;
-                case "10":
-                    resultado = OperacionUnaria(Arcotangente);
-                    break;
-                case "11":
-                    resultado = OperacionBinaria(Exponencial);
-                    break;
-                case "12":
-                    resultado = OperacionBinaria(Logaritmo);
-                    break;
-                case "13":
-                    resultado = OperacionUnaria(RaizCuadrada);
-                    break;
-                default:
-                    Console.WriteLine("Opción no válida. Inténtelo de nuevo.");
-                    break;
-            }
+		switch (opcion)
+		  {
+		  case "1":
 
-            Console.WriteLine($"El resultado es: {resultado}");
-            Console.WriteLine("Presione cualquier tecla para continuar...");
-            Console.ReadKey();
-        }
-    }
+			resultado = OperacionBinaria (Suma);
+			break;
+			case "2":resultado = OperacionBinaria (Resta);
+			break;
+			case "3":resultado = OperacionBinaria (Multiplicacion);
+			break;
+			case "4":resultado = OperacionBinaria (Division);
+			break;
+			case "5":resultado = OperacionUnaria (Seno);
+			break;
+			case "6":resultado = OperacionUnaria (Coseno);
+			break;
+			case "7":resultado = OperacionUnaria (Tangente);
+			break;
+			case "8":resultado = OperacionUnaria (Arcoseno);
+			break;
+			case "9":resultado = OperacionUnaria (Arcocoseno);
+			break;
+			case "10":resultado = OperacionUnaria (Arcotangente);
+			break;
+			case "11":resultado = OperacionBinaria (Exponencial);
+			break;
+			case "12":resultado = OperacionBinaria (Logaritmo);
+			break;
+			case "13":resultado = OperacionUnaria (RaizCuadrada);
+			break;
+			default:Console.
+			  WriteLine ("OpciC3n no vC!lida. IntC)ntelo de nuevo.");
+			break;
+		  }
 
-    static double OperacionBinaria(Func<double, double, double> operacion)
-    {
-        Console.Write("Ingrese el primer número: ");
-        double num1 = LeerNumero();
-        Console.Write("Ingrese el segundo número: ");
-        double num2 = LeerNumero();
-        return operacion(num1, num2);
-    }
 
-    static double OperacionUnaria(Func<double, double> operacion)
-    {
-        Console.Write("Ingrese el número: ");
-        double num = LeerNumero();
-        return operacion(num);
-    }
+		Console.WriteLine ($ "El resultado es: {resultado}");
 
-    static double LeerNumero()
-    {
-        double num;
-        while (!double.TryParse(Console.ReadLine(), out num))
-        {
-            Console.WriteLine("Entrada inválida. Inténtelo de nuevo.");
-        }
-        return num;
-    }
+		Console.WriteLine ("Presione cualquier tecla para continuar...");
 
-    static double Suma(double a, double b)
-    {
-        return a + b;
-    }
+		Console.ReadKey ();
+	  }
 
-    static double Resta(double a, double b)
-    {
-        return a - b;
-    }
+  }
 
-    static double Multiplicacion(double a, double b)
-    {
-        return a * b;
-    }
 
-    static double Division(double a, double b)
-    {
-        if (b != 0)
-            return a / b;
-        else
-        {
-            Console.WriteLine("No se puede dividir por cero.");
-            return double.NaN;
-        }
-    }
+  static double OperacionBinaria (Func < double, double, double >operacion)
+  {
+	Console.Write ("Ingrese el primer nC:mero: ");
+	double num1 = LeerNumero ();
 
-    static double Seno(double a)
-    {
-        return Math.Sin(a);
-    }
+	Console.Write ("Ingrese el segundo nC:mero: ");
+	double num2 = LeerNumero ();
 
-    static double Coseno(double a)
-    {
-        return Math.Cos(a);
-    }
+	return operacion (num1, num2);
+  }
 
-    static double Tangente(double a)
-    {
-        return Math.Tan(a);
-    }
 
-    static double Arcoseno(double a)
-    {
-        return Math.Asin(a);
-    }
+  static double OperacionUnaria (Func < double, double >operacion)
+  {
+	Console.Write ("Ingrese el nC:mero: ");
+	double num = LeerNumero ();
 
-    static double Arcocoseno(double a)
-    {
-        return Math.Acos(a);
-    }
+	return operacion (num);
+  }
 
-    static double Arcotangente(double a)
-    {
-        return Math.Atan(a);
-    }
 
-    static double Exponencial(double a, double b)
-    {
-        return Math.Pow(a, b);
-    }
+  static double LeerNumero ()
+  {
+	double num;
 
-    static double Logaritmo(double a, double b)
-    {
-        return Math.Log(a, b);
-    }
+	while (!double.TryParse (Console.ReadLine (), out num))
+	  {
+		Console.WriteLine ("Entrada invC!lida. IntC)ntelo de nuevo.");
 
-    static double RaizCuadrada(double a)
-    {
-        return Math.Sqrt(a);
-    }
+	  }
+	return num;
+  }
+
+
+  static double Suma (double a, double b)
+  {
+	return a + b;
+  }
+
+
+  static double Resta (double a, double b)
+  {
+	return a - b;
+  }
+
+
+  static double Multiplicacion (double a, double b)
+  {
+	return a * b;
+  }
+
+
+  static double Division (double a, double b)
+  {
+
+	if (b != 0)
+	  return a / b;
+	else
+	  {
+
+		Console.WriteLine ("No se puede dividir por cero.");
+
+		return double.NaN;
+
+	  }
+  }
+
+  static double Seno (double a)
+  {
+
+	return Math.Sin (a);
+  }
+
+
+  static double Coseno (double a)
+  {
+
+	return Math.Cos (a);
+  }
+
+
+  static double Tangente (double a)
+  {
+
+	return Math.Tan (a);
+  }
+
+
+  static double Arcoseno (double a)
+  {
+
+	return Math.Asin (a);
+  }
+
+
+  static double Arcocoseno (double a)
+  {
+
+	return Math.Acos (a);
+  }
+
+
+  static double Arcotangente (double a)
+  {
+
+	return Math.Atan (a);
+  }
+
+
+  static double Exponencial (double a, double b)
+  {
+
+	return Math.Pow (a, b);
+  }
+
+
+  static double Logaritmo (double a, double b)
+  {
+
+	return Math.Log (a, b);
+  }
+
+
+  static double RaizCuadrada (double a)
+  {
+
+	return Math.Sqrt (a);
+  }
+
 }
